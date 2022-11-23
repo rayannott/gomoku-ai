63e105493d0aedb9d8367956c86f4d5929cac891
rayannott <imjobjob@gmail.com>
2022-11-23 20:32:35 +0100

    basic gameplay

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ from environments.freestyle import Board
 board = Board()
 to_place = 1
 
+col_map = {1: 'Black', -1: 'White'}
+
 while True:
     board.display()
-    print(board.last_placed)
-    where = tuple(map(int, input("Enter next pos:").split()))
+    print(col_map[to_place], ', your turn.')
+    where = tuple(map(int, input("Enter next pos: ").split()))
     board.place(where, to_place)
+    is_over = board.is_over()
     to_place *= -1
+    if is_over:
+        print('The winner is: ', col_map[to_place])
