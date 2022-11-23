10a6fadef78e650c343818a91f41e2b55536736f
rayannott <imjobjob@gmail.com>
2022-11-23 20:32:16 +0100

    Board class

--- environments/freestyle.py
+++ environments/freestyle.py
@@ -16,8 +16,9 @@ class Board:
         pass
 
     def display(self) -> None:
+        label = {1: 'B', -1: 'W', 0: '-'}
         for row in self.b:
-            print(' '.join(map(str, row)))
+            print(' '.join([label[el] for el in row]))
 
     def place(self, where: Pos, what: FieldCell) -> None:
         '''
@@ -32,9 +33,39 @@ class Board:
         
         self.last_placed = where
 
+    def cell_on_board(self, pos: Pos):
+        return 0 <= pos[0] < self.board_shape[0] and 0 <= pos[1] < self.board_shape[1]
+
     def is_over(self) -> FieldCell:
         '''
         Checks if the game is over and outputs the winner's color.
         '''
-        r, c = self.last_placed
+        if self.last_placed is None:
+            return 0
         
+        r, c = self.last_placed
+        current_color = self.b[r][c]
+
+        deltas = [(0, 1), (1, 0), (-1, 1), (1, 1)]
+        for d in deltas:
+            streak = 1
+            i, j = 1, -1
+            positive, negative = True, True
+            while True:
+                if positive and self.cell_on_board((r + i*d[0], c + i*d[1])) and self.b[r + i*d[0]][c+i*d[1]] == current_color:
+                    streak += 1
+                    i += 1
+                else:
+                    positive = False
+                if negative and self.cell_on_board((r + j*d[0], c + j*d[1])) and self.b[r + j*d[0]][c+j*d[1]] == current_color:
+                    streak += 1
+                    j -= 1
+                else:
+                    negative = False
+                if not (positive or negative):
+                    break    
+            # print('streak:', d, streak)
+        if streak == 5:
+            return current_color
+        return 0
+
