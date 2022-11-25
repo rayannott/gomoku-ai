9134887d493d027820bb52de4a82ea876be9fab9
rayannott <imjobjob@gmail.com>
2022-11-25 22:34:02 +0100

    added constants and theme.json

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,15 @@
 from enum import Enum
 from typing import Literal
+from os.path import join
+import json
 
 FieldCell = Literal[0, 1, -1]
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 800
+FIELD_SIZE = SCREEN_HEIGHT
+STONE_RADIUS = 14
+THEME_PATH = join('theme.json')
+with open(THEME_PATH) as fr:
+    THEME = json.load(fr)
 
 Pos = tuple[int, int]
